0cc4b0a66f7704a1e12212c6733ba035e758cb40
Юрий Лебедев <yalebedev@alfabank.ru>
2018-01-23 12:53:07 +0300

    Connect catalog module to watch target Add example UI

--- WATCH Extension/New Group/Catalog/CatalogInterfaceController.swift
+++ WATCH Extension/New Group/Catalog/CatalogInterfaceController.swift
@@ -10,15 +10,30 @@ class CatalogInterfaceController: WKInterfaceController {
     let interactor: CatalogBusinessLogic
     var state: Catalog.ViewControllerState
     
-    init(interactor: CatalogBusinessLogic, initialState: Catalog.ViewControllerState = .loading) {
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var catalogTable: WKInterfaceTable!
+    
+    override init() {
+        let presenter = CatalogPresenter()
+        let interactor = CatalogInteractor(presenter: presenter)
         self.interactor = interactor
-        self.state = initialState
+        self.state = .loading
         super.init()
+        presenter.viewController = self
     }
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         display(newState: state)
+    }
+    
+    func setupTable(_ items: [CatalogViewModel]) {
+        catalogTable.setNumberOfRows(items.count, withRowType: "CatalogCell")
+        items.enumerated().forEach { (index, item) in
+            if let row = catalogTable.rowController(at: index) as? CatalogCell {
+                row.titleLabel.setText(item.title)
+            }
+        }
     }
     
     // MARK: Fetching
@@ -37,13 +52,16 @@ extension CatalogInterfaceController: CatalogDisplayLogic {
         state = newState
         switch state {
         case .loading:
+            statusLabel.setText("Loading...")
             fetchItems()
         case let .error(message):
-            print("error \(message)")
+            statusLabel.setText("Error: \(message)")
         case let .result(items):
-            print("result: \(items)")
+            statusLabel.setHidden(true)
+            catalogTable.setHidden(false)
+            setupTable(items)
         case .emptyResult:
-            print("empty result")
+            statusLabel.setText("Empty")
         }
     }
 }
