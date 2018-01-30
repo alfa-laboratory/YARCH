fe8f77691e479bddadbcfc21bd8855856aeb9eaa
Юрий Лебедев <yalebedev@alfabank.ru>
2018-01-30 14:55:43 +0300

    Add tvOS target

---
+++ TV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV
+//
+//  Created by Yuriy Lebedev on 30.01.2018.
+//  Copyright © 2018 Alfa-Bank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
